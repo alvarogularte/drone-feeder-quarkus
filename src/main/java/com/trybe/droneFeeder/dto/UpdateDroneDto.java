2fe7b15c924317ad3213024e773e8d850c94b49a
alvarogularte <alvaro.gularte@gmail.com>
2022-07-15 23:17:05 -0300

    new: create dto classes

---
+++ src/main/java/com/trybe/droneFeeder/dto/UpdateDroneDto.java
@@ -0,0 +1,13 @@
+package com.trybe.droneFeeder.dto;
+
+public class UpdateDroneDto {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
