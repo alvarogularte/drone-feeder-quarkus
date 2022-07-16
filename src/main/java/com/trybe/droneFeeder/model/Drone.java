55cab2722d4092aa447f026848d45053287145f7
alvarogularte <alvaro.gularte@gmail.com>
2022-07-15 22:53:57 -0300

    new: create Drone model

---
+++ src/main/java/com/trybe/droneFeeder/model/Drone.java
@@ -0,0 +1,34 @@
+package com.trybe.droneFeeder.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+@Entity
+public class Drone extends PanacheEntityBase {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
+  private String name;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
